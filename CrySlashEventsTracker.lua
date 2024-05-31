4c2708e46cf278ca5db80bcc8a03622085ba4d5b
Crystofher Lins <crystofher_lins@outlook.com>
2024-05-31 07:48:42 -0300

    tooltip name char event status

--- CrySlashEventsTracker.lua
+++ CrySlashEventsTracker.lua
@@ -1,7 +1,8 @@
 --local playername = UnitName("player");
 --message("Hello " .. playername);
 local _, L = ...
-CryslashEventsTrackerSaved = CryslashEventsTrackerSaved or {}
+--CryslashEventsTrackerSaved = CryslashEventsTrackerSaved or {}
+local charName
 
 SLASH_CEVENTSTRACKER1 = "/cet"
 SLASH_CEVENTSTRACKER2 = "/eventstracker"
@@ -180,90 +181,89 @@ if not region or region > 5 then
     region = 1;
 end
 
--- function ShowEventTooltip(title, status)
---     GameTooltip:SetOwner(self, "ANCHOR_TOPRIGHT")
---     if status then
---         status = string.format("|cff00ff00%s|r",L["Event_is_Active"])
---     else
---         status = string.format("|cffffffff%s|r",L["Event_is_Waiting"])
---     end
---     GameTooltip:SetText(string.format("%s\n".."\n%s",title,status))
---     -- ShowUIPanel(ItemRefTooltip)
---     ShowUIPanel(GameTooltip)
--- end
-
 self.frames = {}
-    for i = 1, 9 do
-        local frame = CreateFrame("Frame", "Event"..i, self, "CryslashEventsTrackerTemplate")   -- UIPanelScrollFrameTemplate CryslashEventsTrackerTemplate
+for i = 1, 9 do
+    local frame = CreateFrame("Frame", "Event"..i, self, "CryslashEventsTrackerTemplate")   -- UIPanelScrollFrameTemplate CryslashEventsTrackerTemplate
+    
+    self.frames[i] = frame
+    if i == 1 then
+        frame:SetPoint("TOPLEFT", self, "TOPLEFT", 35, -95)
+    elseif i == 6 then
+        frame:SetPoint("TOPLEFT", self.frames[1], "BOTTOMLEFT", 0, -10)
+    elseif i == 11 then 
+        frame:SetPoint("TOPLEFT", self.frames[i-5],"BOTTOMLEFT", 0, 0)    
+    else
+        frame:SetPoint("TOPLEFT", self.frames[i-1], "TOPRIGHT", 12, 0)
+    end
+    frame.icon:SetTexture(events[i]["icon"])
+    frame.icon:SetAtlas(events[i]["icon"])
+    frame.tooltipTitle = events[i].name
+    frame.questIDs = events[i].questIDs
 
-        self.frames[i] = frame
-        if i == 1 then
-            frame:SetPoint("TOPLEFT", self, "TOPLEFT", 35, -95)
-        elseif i == 6 then
-            frame:SetPoint("TOPLEFT", self.frames[1], "BOTTOMLEFT", 0, -10)
-        elseif i == 11 then 
-            frame:SetPoint("TOPLEFT", self.frames[i-5],"BOTTOMLEFT", 0, 0)    
+    frame.initComplete = false
+    frame.notification = false
+    frame.isEventActive = nil
+    frame.timeTmp = nil
+    frame.eventNewRegionOffset = events[i].regionOffset [region] > 1600000000 and events[i].regionOffset [region] or (serverResetTime + events[i].regionOffset [region]);
+    
+    --_G["CryslashEventsTrackerSaved"][UnitName("player")].events[i].iscompleted = 1
+    --print(charName)
+    --print(CryslashEventsTrackerSaved[UnitName("player") .. " - " .. GetRealmName()])
+
+    frame:SetScript("OnEnter", function(self)
+        GameTooltip:SetOwner(self, "ANCHOR_RIGHT") --ANCHOR_CURSOR_RIGHT
+        local status
+        local iscompleted = nil
+        if self.isEventActive then
+            status = string.format("|cffffff00%s|r",L["Event_is_Active"]) --amarelo
         else
-            frame:SetPoint("TOPLEFT", self.frames[i-1], "TOPRIGHT", 12, 0)
+            status = string.format("|cffffffff%s|r",L["Event_is_Waiting"])
         end
-        frame.icon:SetTexture(events[i]["icon"])
-        frame.icon:SetAtlas(events[i]["icon"])
-        frame.tooltipTitle = events[i].name
-        frame.questIDs = events[i].questIDs
+        iscompleted = IsQuestCompleted(self.questIDs)
+        if iscompleted then
+            GameTooltip:SetText(string.format("|cff00ffff%s|r\n",self.tooltipTitle))
+            CryslashEventsTrackerSaved[charName].events[i] = 1
+            GameTooltip:AddLine(string.format("|cff00ff00%s|r",charName)) --verde
+        else
+            --GameTooltip:SetText(string.format("%s\n\n |cff00ff00Disponível Nesse Char|r".."\n\n%s",self.tooltipTitle,status))
+            GameTooltip:SetText(string.format("|cff00ffff%s|r\n",self.tooltipTitle))
+            CryslashEventsTrackerSaved[charName].events[i] = 0
+            GameTooltip:AddLine(string.format("|cffff0000%s|r",charName)) --vermelho
+        end
 
-        frame.initComplete = false
-        frame.notification = false
-        frame.isEventActive = nil
-        frame.timeTmp = nil
-        frame.eventNewRegionOffset = events[i].regionOffset [region] > 1600000000 and events[i].regionOffset [region] or (serverResetTime + events[i].regionOffset [region]);
+        for char in pairs(CryslashEventsTrackerSaved) do
+            if char ~= charName then
+                if CryslashEventsTrackerSaved[char].events[i] == 1 then
+                    GameTooltip:AddLine(string.format("|cff00ff00%s|r",char)) --verde
+                elseif CryslashEventsTrackerSaved[char].events[i] == 0 or CryslashEventsTrackerSaved[char].events[i] == nil then
+                    GameTooltip:AddLine(string.format("|cffff0000%s|r",char)) -- vermelho
+                end
+            end
+        end
         
-        frame:SetScript("OnEnter", function(self)
-            GameTooltip:SetOwner(self, "ANCHOR_RIGHT") --ANCHOR_CURSOR_RIGHT
-            local status
-            local iscompleted = nil
-            if self.isEventActive then
-               status = string.format("|cffffff00%s|r",L["Event_is_Active"]) --amarelo
-            else
-               status = string.format("|cffffffff%s|r",L["Event_is_Waiting"])
-            end
-            iscompleted = IsQuestCompleted(self.questIDs)
-            if iscompleted then
-                --GameTooltip:SetText(string.format("%s\n\n |cffff0000Completo Nesse Char|r".."\n\n%s",self.tooltipTitle,status))
-                GameTooltip:SetText(string.format("|cff00ffff%s|r",self.tooltipTitle))
-                GameTooltip:AddLine("\n|cff00ff00Concluído|r") --verde
-            else
-                --GameTooltip:SetText(string.format("%s\n\n |cff00ff00Disponível Nesse Char|r".."\n\n%s",self.tooltipTitle,status))
-                GameTooltip:SetText(string.format("|cff00ffff%s|r",self.tooltipTitle))
-                GameTooltip:AddLine("\n|cffff0000Não concluído|r")
-            end            
-            GameTooltip:AddLine(status)
-            GameTooltip:Show()
-            ShowUIPanel(GameTooltip)
-            end
-        )
+        GameTooltip:AddLine("\n"..status)
+        GameTooltip:Show()
+        ShowUIPanel(GameTooltip)
+    end)
 
-        frame:SetScript("OnUpdate", function(self, elapsed)
-		if self.initComplete then
-			self.elapsed = (self.elapsed or 1) + elapsed;
-			if self.elapsed < 1 then return end
-			self.elapsed = 0;
-		end
-
-        if not self:IsShown() then return end
-
+    frame:SetScript("OnUpdate", function(self, elapsed)
+	if self.initComplete then
+		self.elapsed = (self.elapsed or 1) + elapsed;
+		if self.elapsed < 1 then return end
+		self.elapsed = 0;
+	end
+    if not self:IsShown() then return end
         local timeFromFirstStart = GetServerTime() - self.eventNewRegionOffset;
         local timeToNextEvent = (events[i].interval - timeFromFirstStart % events[i].interval) + (self.timeTmp and self.timeTmp or 0);
         local timeActiveEvent = self.timeTmp and timeToNextEvent or events[i].duration - (events[i].interval - timeToNextEvent);
         local isEventActive = (events[i].interval + (self.timeTmp and self.timeTmp or 0)) - timeToNextEvent <= events[i].duration;
         local showedTime = isEventActive and timeActiveEvent or timeToNextEvent;
-
      
         if not self.initComplete or (self.isEventActive and not isEventActive) then
             if (self.isEventActive and not isEventActive) then self.isEventActive = false end
             self.timer:SetTextColor(.9, .9, .9); --(.9, .9, .9)  branco
             self.timeTmp = nil;
         end
-
         if not self.isEventActive and isEventActive then
             if self.timeTmp and self.timeTmp > 0 then
                 self.timeTmp = self.timeTmp - events[i].interval;
@@ -283,31 +283,33 @@ self.frames = {}
             if self.anim and not self.icon.Anim:IsPlaying() then
                 self.icon.Anim:Play();
             end
-        
-        -- elseif not isEventActive then
-        --     if showedTime < 300 and not self.timeTmp then
-        --         if not self.notification then
-        --             if self.initComplete then
-        --                 PlaySound(32585, "Master");
-        --             end
-        --             if self.initComplete then 
-        --                 --C_VoiceChat.SpeakText(0, events[i].name.." event will start in a moment.", 1, 0, 50);
-        --                 C_VoiceChat.SpeakText(0, events[i].name.." começará em breve.", 1, 0, 70);
-        --             end
-        --             -- if self.anim and not self.icon.Anim:IsPlaying() then
-        --             --     self.icon.Anim:Play();
-        --             -- end
-        --             --self.timer:SetTextColor(.1, .6, .8); -- (1, 1, 0) amarelo
-        --             self.timer:SetTextColor(.9, .9, .9); -- (1, 1, 0) amarelo
-        --             self.notification = true;
-        --         end
-        --     elseif self.notification then
-        --         self.notification = false;
-        --     end
         end
         self.timer:SetText(SecondsToEventTime(showedTime))
         self.initComplete = true
     end)
-    end
+
+end
+
+    self:RegisterEvent("ADDON_LOADED", CEventsTracker_OnEvent)
     MakeMovable(CEventsTracker)
+end
+
+function CEventsTracker_OnEvent(self, event, ...)
+    if event == "ADDON_LOADED" and ... == "CrySlash_EventsTracker" then
+	    charName = UnitName("player") .. " - " .. GetRealmName();
+		if CryslashEventsTrackerSaved == nil then
+            CryslashEventsTrackerSaved = {}
+        end
+		if CryslashEventsTrackerSaved[charName] == nil then
+			CryslashEventsTrackerSaved[charName] = {}
+			CryslashEventsTrackerSaved[charName].class = select(2, UnitClass("player"))
+			CryslashEventsTrackerSaved[charName].events = {}
+		end    
+    elseif event == "PLAYER_LOGOUT" then    
+    
+        
+        
+    elseif event == "PLAYER_ENTERING_WORLD" then
+        CryslashEventsTrackerSaved = CryslashEventsTrackerSaved or 0
+    end
 end
